969a3a38ab9fc09758782d0b62eea5c1dba91741
Allan Fernandes <fernzallan14@gmail.coms>
2020-07-23 13:52:17 +0530

    Updates

---
+++ precache-manifest.a21d820674893b8ec0ccba8d2cd1b770.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4813e7ed19e84faef35c8030d660f5b9",
+    "url": "/monstersRola/index.html"
+  },
+  {
+    "revision": "0f4c160bcc4e3abd37b6",
+    "url": "/monstersRola/static/css/main.9f968a9a.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monstersRola/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstersRola/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f4c160bcc4e3abd37b6",
+    "url": "/monstersRola/static/js/main.552f3553.chunk.js"
+  },
+  {
+    "revision": "535394988b3d1b5ec7f0",
+    "url": "/monstersRola/static/js/runtime-main.4bd3b442.js"
+  }
+]);
